19602fa8f657c36af465cd587dfd95531d2d06f7
Ivan Bogatyrev <adamether@yandex-team.ru>
2020-08-14 16:10:41 +0300

    Update TupleIterators.java

--- src/main/java/com/adamether/TupleIterators.java
+++ src/main/java/com/adamether/TupleIterators.java
@@ -16,34 +16,19 @@ public class TupleIterators {
             return Collections.emptyIterator();
         }
 
-        // Create min heap of total streams size
-        // S: O(m * p * q)
         Queue<HeapNode> minHeap = new PriorityQueue<>(
                 iterators.size(),
                 (o1, o2) -> Tuple.compare(o1.getTuple(), o2.getTuple())
         );
 
-        // Insert first tuples from each iterator into min heap
-        // T: O(m) * O(log(m))
         for (Iterator<Tuple> iterator : iterators) {
             if (iterator.hasNext()) {
                 minHeap.offer(new HeapNode(iterator, iterator.next()));
             }
         }
 
-        // Create result list witch will contain combined streams maintaining the ordering, and without duplicates.
-        // Choosing LinkedList for fastest insert speed.
-        // For ArrayList insert time complexity is O(n) in the worst case due to array resizing.
-        // S: O(m * n * p * q)
         LinkedList<Tuple> mergedTuples = new LinkedList<>();
 
-        // Run the loop                   | O(m * n)
-        // Poll from min heap             | O(log(m))
-        // Get the last item from result  | O(1)
-        // Call tuple equals              | O(p * q)
-        // Insert into result             | O(1)
-        // Insert into min heap           | O(log(m))
-        // T                              | O(m * n * p * q * log(m))
         while (!minHeap.isEmpty()) {
             HeapNode heapNode = minHeap.poll();
             Tuple minTuple = heapNode.getTuple();
@@ -59,15 +44,6 @@ public class TupleIterators {
         }
 
         return mergedTuples.iterator();
-
-        // Complexity analyse
-        // T: O(m * n * p * q * log(m))
-        // S: O(m * n * p * q)
-
-        // Further optimization
-        // We can enhance Tuple#equals method time complexity to constant time by comparing hash strings
-        // instead of iterating through each value bytes.
-        // (Hashes calculated by MD5 / SHA1 algorithm during tuple construction)
     }
 
     static class HeapNode {
